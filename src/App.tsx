34f273434b1f7e49c40bbcd588a515c01a1a3d3a
kamal heydari <kamal3heydari@gmail.com>
2023-01-14 17:04:29 +0330

    feat contact us

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { SelectedPage } from "@/shared/types";
 
 import Home from "@/scenes/home";
 import Navbar from "@/scenes/navbar";
-import Benefits from "./scenes/benefits";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -31,6 +34,9 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
