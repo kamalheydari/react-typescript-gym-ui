710195a48e1a623abacdc47b532e8e3f348fac42
kamal heydari <kamal3heydari@gmail.com>
2023-01-14 14:53:09 +0330

    feat navbar

---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,20 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+const ActionButton = ({ setSelectedPage, children }: Props) => {
+  return (
+    <AnchorLink
+      className='rounded-md bg-secondary-500 px-10 py2 hover:bg-primary-500 hover:text-white transition-colors'
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
